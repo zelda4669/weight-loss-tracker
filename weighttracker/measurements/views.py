b233290dd9cd218425b0b510f156d66b80a03d15
Noelle Ferrari <ferrari.noelle@gmail.com>
2022-07-01 14:39:18 -0700

    Add initial html templates

--- weighttracker/measurements/views.py
+++ weighttracker/measurements/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def index(request):
-    return HttpResponse('Enter your stats here.')
-
-# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'home.html'
--- weighttracker/weighttracker/urls.py
+++ weighttracker/weighttracker/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('measurements/', include('measurements.urls')),
+    path('', include('measurements.urls')),
 ]
