6cf6d3d420d6427590053ba0f61757b8a86b23b7
Noelle Ferrari <ferrari.noelle@gmail.com>
2022-07-27 14:11:36 -0700

    Add additional styling

--- body_comp/body_comp/settings.py
+++ body_comp/body_comp/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #local
     'accounts.apps.AccountsConfig',
-    'measurements.apps.MeasurementsConfig'
+    'measurements.apps.MeasurementsConfig',
+
+    #third party
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +137,6 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
+
+CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
