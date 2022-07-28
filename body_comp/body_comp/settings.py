0f1c28060f079b63e5bd18ad577f8930d49bab80
Noelle Ferrari <ferrari.noelle@gmail.com>
2022-07-27 21:46:16 -0700

    Add forgotten password flow

--- body_comp/body_comp/settings.py
+++ body_comp/body_comp/settings.py
@@ -141,4 +141,4 @@ LOGOUT_REDIRECT_URL = 'login'
 CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
