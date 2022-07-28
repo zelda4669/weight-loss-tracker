80899112f8c6219b276a6eeb2ca6e9326f7e3c3e
Noelle Ferrari <ferrari.noelle@gmail.com>
2022-07-27 20:28:56 -0700

    Add password change forms

--- body_comp/accounts/views.py
+++ body_comp/accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
+from .models import CustomUser
 
 from .forms import CustomUserCreationForm
 
@@ -10,3 +11,8 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
+
+
+class SettingsView(ListView):
+    template_name = 'settings.html'
+    model = CustomUser
